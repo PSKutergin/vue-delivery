f80ad161fb5093305674ff6e4da7724f9d7949ea
PSKutergin <PSKutergin@gmail.com>
2024-01-14 14:52:28 +0300

    upd settings

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/delivery-vue-2/",
+  base: "/vue-delivery/",
   plugins: [
     vue(),
   ],
